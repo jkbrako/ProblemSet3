4758b474fbe26a974a75fd1797f855f0b14bd6b9
Jamin Brako <jbrako@ucvts.tec.nj.us>
2020-03-06 11:22:58 -0500

    first commit

---
+++ src/problem/set/pkg3/ProblemSet3.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problem.set.pkg3;
+ import java.util.Scanner;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author jbrako
+ */
+public class ProblemSet3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /**
+         * • Accept at least 1 input, to be parsed as a String
+         *
+         * • Accept at least 1 input, to be parsed as an int
+         *
+         * • Accept at least 1 input, to be parsed as a double
+         *
+         * • Use at least 1 input in a question for the user
+         *
+         * • Do math with at least 1 int input
+         *
+         * • Do math with at least 1 double input
+         *
+         * • Accept at least 10 total inputs
+         *
+         */
+        String inputString = JOptionPane.showInputDialog("You need to type something.");
+        int inputIn = Integer.parseInt(inputString);
+        inputIn++;
+        
+        double inputDo = 
+    }
+    
+}
